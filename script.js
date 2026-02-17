ae9afcae043485a4c3fe192e10a703beb5d0d59a
anupom998 <anupom11118@gmail.com>
2026-02-17 19:53:16 +0600

    homepage layout complete

---
+++ script.js
@@ -0,0 +1,9 @@
+const mobileMenuBtn = document.getElementById("mobile-menu-btn");
+
+const mobileMenu = document.getElementById("mobile-menu");
+
+mobileMenuBtn.addEventListener("click", () => {
+    
+    mobileMenu.classList.toggle("hidden");
+    mobileMenu.classList.toggle("flex");
+})
